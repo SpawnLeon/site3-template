0b3d1b4e1b9de2c9d953883d9fecaaf6be1a78cb
Alexander Leonov <spawnleon@gmail.com>
2020-06-25 17:55:27 +0300

    public path

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,7 @@ const config = {
   entry: ['./src/js/index.js', './src/scss/style.scss'],
   output: {
     filename: './js/bundle.js',
+    publicPath: '/itorum-ural2/dist',
   },
   devtool: 'source-map',
   mode: 'production',

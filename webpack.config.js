a86b7e1de86ab4ce5b10a49791fb5c8244aac6f8
Alexander Leonov <spawnleon@gmail.com>
2020-06-25 17:26:28 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,134 @@
+const path = require('path');
+const fs = require('fs');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
+
+function generateHtmlPlugins (templateDir) {
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+  return templateFiles.map((item) => {
+    const parts = item.split('.');
+    const name = parts[0];
+    const extension = parts[1];
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+      inject: true,
+    });
+  });
+}
+
+const htmlPlugins = generateHtmlPlugins('./src/html/views');
+
+const config = {
+  entry: ['./src/js/index.js', './src/scss/style.scss'],
+  output: {
+    filename: './js/bundle.js',
+  },
+  devtool: 'source-map',
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true,
+        extractComments: true,
+      }),
+    ],
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              url: false,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: `./postcss.config.js`,
+              },
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {},
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        include: path.resolve(__dirname, 'src/html/includes'),
+        use: [
+          {
+            loader: 'raw-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/style.bundle.css',
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './src/fonts',
+        to: './fonts',
+      },
+      {
+        from: './src/favicon',
+        to: './favicon',
+      },
+      {
+        from: './src/img',
+        to: './img',
+      },
+      {
+        from: './src/uploads',
+        to: './uploads',
+      },
+    ]),
+    new SVGSpritemapPlugin(
+      'src/img/svg/**/*.svg',
+      {
+        output: {
+          filename: 'img/spritemap.svg'
+        }
+
+      }
+    )
+  ].concat(htmlPlugins),
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'production') {
+    config.plugins.push(new CleanWebpackPlugin());
+  }
+  return config;
+};

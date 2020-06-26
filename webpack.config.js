1049e822f8bc8709081b9b41ec14ed34ab8807e5
Alexander Leonov <spawnleon@gmail.com>
2020-06-26 11:53:10 +0300

    finish main page desktop

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ const config = {
   entry: ['./src/js/index.js', './src/scss/style.scss'],
   output: {
     filename: './js/bundle.js',
-    publicPath: './',
+    //publicPath: './',
   },
   devtool: 'source-map',
   mode: 'production',

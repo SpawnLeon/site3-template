4dd617ac55d8ae16617d373b38b479d9779a5220
Alexander Leonov <spawnleon@gmail.com>
2020-07-10 17:59:57 +0300

    next stage

--- src/js/index.js
+++ src/js/index.js
@@ -11,74 +11,76 @@ import '@fancyapps/fancybox';
 import Swiper from 'swiper';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Vue init
-  // const app = new Vue({
-  //   //el: '#app',
-  // });
+    // Vue init
+    // const app = new Vue({
+    //   //el: '#app',
+    // });
 
-  new Swiper('.main-slider__container', {
-    loop: true,
-  });
-
-  Array.from(document.querySelectorAll('.item-form__field--text')).forEach((el, i) => {
-    el.addEventListener('focus', function (event) {
-      this.nextElementSibling.classList.add('item-form__name--focus');
+    new Swiper('.main-slider__container', {
+        loop: true,
     });
-    el.addEventListener('blur', function (event) {
-      if (!this.value) {
-        this.nextElementSibling.classList.remove('item-form__name--focus');
-      }
+
+    Array.from(document.querySelectorAll('.item-form__field--text')).forEach((el, i) => {
+        el.addEventListener('focus', function (event) {
+            this.nextElementSibling.classList.add('item-form__name--focus');
+        });
+        el.addEventListener('blur', function (event) {
+            if (!this.value) {
+                this.nextElementSibling.classList.remove('item-form__name--focus');
+            }
+        });
+        if (el.value) {
+            el.nextElementSibling.classList.add('item-form__name--focus');
+        }
     });
-    if (el.value) {
-      el.nextElementSibling.classList.add('item-form__name--focus');
-    }
-  });
 
 
-  //custom select
-  document.querySelectorAll('.item-form--select').forEach((el, i) => {
-    const input = el.querySelector('.item-form__field');
-    const inputHidden = el.querySelector('.dropdown-form__input');
-    const dropdown = el.querySelector('.dropdown-form');
-    document.addEventListener('click', function (event) {
-      if (event.target !== input) {
-        dropdown.classList.add('dropdown-form--hidden');
-      }
+    //custom select
+    document.querySelectorAll('.item-form--select').forEach((el, i) => {
+        const input = el.querySelector('.item-form__field');
+        const inputHidden = el.querySelector('.dropdown-form__input');
+        const dropdown = el.querySelector('.dropdown-form');
+        document.addEventListener('click', function (event) {
+            if (event.target !== input) {
+                dropdown.classList.add('dropdown-form--hidden');
+            }
+        });
+        input.addEventListener('focus', function (event) {
+            dropdown.classList.remove('dropdown-form--hidden');
+        });
+        dropdown.querySelectorAll('.dropdown-form__item').forEach((el, i) => {
+            el.addEventListener('mousedown', function (event) {
+                const value = this.dataset.value;
+                const title = this.textContent;
+                inputHidden.value = value;
+                input.value = title;
+                input.focus();
+            });
+        });
     });
-    input.addEventListener('focus', function (event) {
-      dropdown.classList.remove('dropdown-form--hidden');
+
+
+    //custom tabs
+
+    document.querySelectorAll('[data-tab-target]').forEach((el, i) => {
+        el.addEventListener('change', function (event) {
+            event.preventDefault();
+            document.querySelectorAll('[data-tab]').forEach((el, i) => {
+                el.hidden = true;
+            });
+
+            const target = this.dataset.tabTarget;
+            const targetTab = document.querySelector(`[data-tab="${target}"]`);
+            if (targetTab) {
+                targetTab.hidden = false;
+            }
+        });
+
+
+
+
+
     });
-    dropdown.querySelectorAll('.dropdown-form__item').forEach((el, i) => {
-      el.addEventListener('mousedown', function (event) {
-        const value = this.dataset.value;
-        const title = this.textContent;
-        inputHidden.value = value;
-        input.value = title;
-        input.focus();
-      });
-    });
-  });
-
-
-  //custom tabs
-
-  document.querySelectorAll('[data-tab-target]').forEach((el, i) => {
-    el.addEventListener('change', function(event) {
-      event.preventDefault();
-      document.querySelectorAll('[data-tab]').forEach((el, i) => {
-        el.hidden = true;
-      })
-
-      const target = this.dataset.tabTarget;
-      const targetTab = document.querySelector(`[data-tab="${target}"]`);
-      if (targetTab) {
-        targetTab.hidden = false;
-      }
-    })
-
-
-
-  })
 });
 
 
